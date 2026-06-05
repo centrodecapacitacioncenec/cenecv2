b808316b9ff558a7f20d16cc02c234e353369e1d
centrodecapacitacioncenec <centrodecapacitacioncenec01@gmail.com>
2026-06-05 12:50:42 -0400

    Update beca.js

--- beca.js
+++ beca.js
@@ -5,28 +5,17 @@ form.addEventListener("submit", async (e) => {
     e.preventDefault();
 
     const data = new URLSearchParams();
-
     data.append("nombre", form.nombre.value);
     data.append("celular", form.celular.value);
     data.append("carta", form.carta.value);
 
-    try {
+    await fetch("TU_URL_EXEC", {
+        method: "POST",
+        mode: "no-cors",
+        body: data
+    });
 
-        await fetch("https://script.google.com/macros/s/AKfycbxHpfgOvE_7jg5hfbyIdOINqfxyXlJSvMadIxWnCKP1NWUGCqLJvi3F1amjrdeD1m4n/exec", {
-            method: "POST",
-            mode: "no-cors",
-            body: data
-        });
-
-        alert("Solicitud enviada correctamente");
-
-        form.reset();
-
-    } catch (error) {
-
-        console.error("Error al enviar:", error);
-        alert("Error al enviar la solicitud");
-
-    }
+    alert("Enviado");
+    form.reset();
 
 });
