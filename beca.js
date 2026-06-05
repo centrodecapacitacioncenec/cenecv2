3931814d5199dc4f6a419629d615829c3cd2796f
centrodecapacitacioncenec <centrodecapacitacioncenec01@gmail.com>
2026-06-05 12:35:20 -0400

    Update beca.js

--- beca.js
+++ beca.js
@@ -4,26 +4,19 @@ form.addEventListener("submit", async (e) => {
 
     e.preventDefault();
 
-    const data = new URLSearchParams();
-    data.append("nombre", form.nombre.value);
-    data.append("celular", form.celular.value);
-    data.append("carta", form.carta.value);
+    const data = {
+        nombre: form.nombre.value,
+        celular: form.celular.value,
+        carta: form.carta.value
+    };
 
-    try {
+    await fetch("https://script.google.com/macros/s/AKfycbxHpfgOvE_7jg5hfbyIdOINqfxyXlJSvMadIxWnCKP1NWUGCqLJvi3F1amjrdeD1m4n/exec", {
+        method: "POST",
+        mode: "no-cors",
+        body: JSON.stringify(data)
+    });
 
-        await fetch("https://script.google.com/macros/s/AKfycbxHpfgOvE_7jg5hfbyIdOINqfxyXlJSvMadIxWnCKP1NWUGCqLJvi3F1amjrdeD1m4n/exec", {
-            method: "POST",
-            mode: "no-cors",
-            body: data
-        });
-
-        alert("Enviado");
-
-        form.reset();
-
-    } catch (err) {
-        console.error(err);
-        alert("Error al enviar");
-    }
+    alert("Enviado");
+    form.reset();
 
 });
